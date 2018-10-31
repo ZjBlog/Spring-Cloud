3c59a60f00d099f88f16cb3c8daec9f713e9bba3
zhangjunTracy <13552960783@163.com>
2018-10-31 22:15:24 +0800

    init

---
+++ eureka-client-user/src/main/java/eureka/client/user/EurekaClientUserApplication.java
@@ -0,0 +1,12 @@
+package eureka.client.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientUserApplication.class, args);
+    }
+}
