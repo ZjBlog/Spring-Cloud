6458c7f029c516793087dc2e0aa4531b2b04dceb
zhangjunTracy <13552960783@163.com>
2018-11-01 22:28:22 +0800

    d

--- eureka-client-user/src/main/java/eureka/client/user/controller/TestController.java
+++ eureka-client-user/src/main/java/eureka/client/user/controller/TestController.java
@@ -1,6 +1,7 @@
 package eureka.client.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,9 @@ public class TestController {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
 
     @GetMapping("/user")
     public String test(){
