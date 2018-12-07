cffffbb9a4e361e1f1a7bef51695c3c7ddc490d2
zj <13552960783@163.com>
2018-12-07 18:10:55 +0800

    config

--- eureka-client-user/src/main/java/eureka/client/user/EurekaClientUserApplication.java
+++ eureka-client-user/src/main/java/eureka/client/user/EurekaClientUserApplication.java
@@ -4,6 +4,7 @@ import eureka.client.user.Config.ExcudeAnnotation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
